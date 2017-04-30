f728f201a8aed66c9ea2916dc66b551fc11073b4
mr-bmv <mr.bmv@list.ru>
2017-04-30 13:40:27 +0300

    Create ArrayList.java
    Get 'n' like a rows in list. Get 'm' rows and move to the end of list.

---
+++ ArrayList.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList  list = new ArrayList ();
+        int N = Integer.parseInt(reader.readLine());
+        int M = Integer.parseInt(reader.readLine());
+        for (int i = 0; i < N; i++) {list.add(reader.readLine());}
+        for (int i = 0; i < M; i++) {list.add(list.get(0)); list.remove(0);}
+        for (int i = 0; i < list.size(); i++) {System.out.println(list.get(i)); }
+    }
+}
