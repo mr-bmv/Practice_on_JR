ff3d5ab313d9b2a71540568e3cf7b87538ac1f10
Mike <mr.bmv@list.ru>
2017-08-31 14:54:09 +0300

    Create 1_2.java
    Выводит на на экран надпись определенное количество раз. Используется цикл for

---
+++ 1_2.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public static void main(String[] args) {
+        String s = "Hello World!!!";
+        for (int i = 0; i <= 4; i++) {
+        System.out.println(s);
+        }
+    }
+}
