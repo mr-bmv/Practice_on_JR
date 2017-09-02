4d3dbd6a03e49dfc243162995ec55b8210c086a7
Mike <mr.bmv@list.ru>
2017-09-02 15:00:18 +0300

    Create 1_4.java
    Программа округляет введенное пользователем число. Без использования Math.round
    Пример: Введенное число = 1.49999999_____________________Округленное число = 1

---
+++ Kostin/1_4.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Программа округляет введенное пользователем число. Без использования 
+Math.round
+*/
+
+class Okruglenie {
+  public static void main(String[] args) throws Exception {
+    System.out.println("Введите вещественное число и нажмите Enter");
+    System.out.println("************************************");
+    BufferedReader bred = new BufferedReader(new InputStreamReader(System.in));
+    double a = Double.parseDouble(bred.readLine());
+    System.out.println(" ");
+    System.out.println("Введенное число = " + a);
+    int b = (int) a ;
+    if ( a < (b + 0.5)) {
+        System.out.println("************************************");
+        System.out.println("Округленное число = " + b);
+    } else {
+      System.out.println("************************************");
+      System.out.println("Округленное число = " + (b + 1));
+    }
+  }
+}
