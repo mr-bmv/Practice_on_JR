ad8860b8890464a65dfec7d0cbaea75a08b7d421
Mike <mr.bmv@list.ru>
2017-09-05 09:36:05 +0300

    Create 7_3.java
    Программа выводит на экран все неотрицательные элементы последовательности 90 85 80 75 70 65 60

---
+++ Kostin/7_3.java
@@ -0,0 +1,12 @@
+/*
+Программа выводит на экран все неотрицательные элементы
+последовательности 90 85 80 75 70 65 60 ….
+*/
+
+class JustPossitivLoop {
+  public static void main(String[] args) {
+    for (int i=90; i >=0; i-=5){
+      System.out.println(i);
+    }
+  }
+}
