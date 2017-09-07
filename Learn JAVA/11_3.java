a5e890305e6c81bd34f5a774f03632b54c5f603f
Mike <mr.bmv@list.ru>
2017-09-07 12:26:58 +0300

    Create 11_3.java
    Программа суммирует числа от 1 до 10

---
+++ Learn JAVA/11_3.java
@@ -0,0 +1,13 @@
+/*
+Программа суммирует числа от 1 до 10
+*/
+
+class SumOfTen{
+  public static void main(String[] args) {
+    int sum =0;
+    for ( int i = 1; i <=10; i++){
+      sum+=i;
+    }
+    System.out.println(sum);
+  }
+}
